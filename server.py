f71c7af17436e0b39eb0c5460c7a3dd423f4d0ac
dimakupriyanov0303-dotcom <dima.kupriyanov.0303@gmail.com>
2026-04-14 17:25:30 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder="webapp")
 
@@ -6,4 +7,10 @@ app = Flask(__name__, static_folder="webapp")
 def home():
     return send_from_directory("webapp", "index.html")
 
-app.run(host="0.0.0.0", port=5000)
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory("webapp", path)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
