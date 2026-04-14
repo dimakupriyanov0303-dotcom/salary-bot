9e7863e3a7544b00e8ab577197ff67228437f392
dimakupriyanov0303-dotcom <dima.kupriyanov.0303@gmail.com>
2026-04-14 17:51:45 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,11 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder="webapp")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return send_from_directory("webapp", "index.html")
-
-@app.route("/<path:path>")
-def static_files(path):
-    return send_from_directory("webapp", path)
+    return "BOT IS RUNNING"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
